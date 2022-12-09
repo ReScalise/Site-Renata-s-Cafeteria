7a16f4f6466db3484b9f8ca5bf675a4b0f577c66
Renata Scalise <115908722+ReScalise@users.noreply.github.com>
2022-12-08 23:38:54 -0300

    commit dia 8

---
+++ SRC/js/produtos.js
@@ -0,0 +1,30 @@
+const produtos= [
+    {
+        id: 1,
+        title:'Produto 01',
+        description: 'Descrição do Produto 01',
+        value: 10,00 
+    },
+
+    {
+        id: 2,
+        title:'Produto 02',
+        description: 'Descrição do Produto 02',
+        value: 20,00
+    },
+
+    {
+        id: 3,
+        title:'Produto 03',
+        description: 'Descrição do Produto 03',
+        value: 30,00
+    },
+
+    {
+        id: 4,
+        title:'Produto 04',
+        description: 'Descrição do Produto 04',
+        value: 40,00
+    },
+
+]
