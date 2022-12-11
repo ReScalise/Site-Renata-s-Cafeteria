4e35d2ecda1a311ed2f1a862a26ce1930ccdc465
Renata Scalise <115908722+ReScalise@users.noreply.github.com>
2022-12-10 23:07:03 -0300

    commit dia 10 2

--- SRC/js/app.js
+++ SRC/js/app.js
@@ -1,9 +1,81 @@
-const loadProducts = (produtos, productId ) => {
-const parentDiv = document.querySelector(productId)
-produtos.array.forEach(produto => {
-    
-
-
-    parentDiv.incertAdjacentHTML ('beforeend')
-});
+const loadProducts = (products, divId) => {
+  const parent = document.querySelector(divId)
+  products.forEach(product => {
+    const html = `
+      <article class="prato">
+        <img src="${product.image}" alt="${product.title}">
+        <h4>${product.title}</h4>
+        <h4>R$ ${product.value}</h4>
+        <p>${product.description}</p>
+        <button type="button" onclick="modalTrigger(${product.id})">Quero este prato</button>
+      </article>
+    `
+    parent.insertAdjacentHTML('beforeend', html)
+  })
 }
+
+const modalTrigger = (productId) => {
+    const modal = document.querySelector('.modal')
+    
+    if (productId != null) {
+      const product = produtos.filter( product => product.id == productId)[0]
+      modal.querySelector('#title').value = product.title
+    }
+  
+    modal.classList.contains('hide') == true ? modal.classList.remove('hide') : modal.classList.add('hide')
+  }
+  
+  const whatsappLinkGenerator = (phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment) => {
+    const baseUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá eu quero: ${productQuantity} ${productTitle} - Entregar para ${buyerName} no endereco: ${buyerAddress} - A forma de pagamento será: ${buyerPayment}`
+    window.location.href = baseUrl
+  }
+  
+  const checkout = (phoneNumber) => {
+    const form = document.querySelector('#form-product')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const productTitle = form.querySelector('input#title').value
+      const productQuantity = form.querySelector('input#quantity').value
+      const buyerName = form.querySelector('input#name').value
+      const buyerAddress = form.querySelector('input#address').value
+      const buyerPayment = form.querySelector('select#payment_type').value
+      console.log(productTitle, productQuantity, buyerName, buyerAddress, buyerPayment)
+      whatsappLinkGenerator(phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment)
+    })
+  }
+  
+  const search = (products, searchTerm) => products.filter( product => product.title.includes(`${searchTerm}`) || product.description.includes(`${searchTerm}`)) 
+  
+  const loadSearch = (form, productsDivId) => {
+    const input = form.querySelector('#inputSearch')
+    const productsDiv = document.querySelector(productsDivId)
+  
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      if (input.value != '' && input.value != null) {
+        productsDiv.querySelectorAll('.prato').forEach(prato => {
+          prato.remove()
+        })
+  
+        let results = search(produtos, input.value)
+        results.forEach(product => {
+          const html = `
+            <article class="prato">
+              <img src="${product.image}" alt="${product.title}">
+              <h4>${product.title}</h4>
+              <h4>R$ ${product.value}</h4>
+              <p>${product.description}</p>
+              <button type="button" onclick="modalTrigger(${product.id})">Quero este prato</button>
+            </article>
+          `
+          productsDiv.insertAdjacentHTML('beforeend', html)
+        })
+      }
+    })
+  }
+  
+  loadProducts(produtos, '#pratos-div')
+  checkout('5596984020861')
+  
+  const form = document.querySelector('#formSearch')
+  loadSearch(form, '#pratos-div', '#buscas-div')
--- SRC/js/produtos.js
+++ SRC/js/produtos.js
@@ -1,34 +1,44 @@
-const produtos= [
+let produtos = [
     {
-        id: 1,
-        Image: https://picsum.photos/200
-        title:'Produto 01',
-        description: 'Descrição do Produto 01',
-        value: 10,00 
+      id: 1,
+      image: 'https://images.pexels.com/photos/5620272/pexels-photo-5620272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title: 'Café Especial',
+      description: 'Café coado, Café Arábica graos selecionados,torra media',
+      value: 5.00
     },
-
     {
-        id: 2,
-        Image: https://picsum.photos/200
-        title:'Produto 02',
-        description: 'Descrição do Produto 02',
-        value: 20,00
+      id: 2,
+      image: 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title: 'Panquecas com Cobertura',
+      description: 'Panquecas americans com coberturas variadas.',
+      value: 20.00
     },
-
     {
-        id: 3,
-        Image: https://picsum.photos/200
-        title:'Produto 03',
-        description: 'Descrição do Produto 03',
-        value: 30,00
+      id: 3,
+      image: 'https://images.pexels.com/photos/775878/pexels-photo-775878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://images.pexels.com/photos/5620272/pexels-photo-5620272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title: 'Cappuccino ',
+      description: 'Cappuccino cremoso, quente ou gelado, com coberturas de creme.',
+      value: 15.00
     },
-
     {
-        id: 4,
-        Image: https://picsum.photos/200
-        title:'Produto 04',
-        description: 'Descrição do Produto 04',
-        value: 40,00
+      id: 4,
+      image: 'https://images.pexels.com/photos/14001482/pexels-photo-14001482.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title: 'Hamburguer com Fritas',
+      description: 'Hamburguer com batatas fritas e molho especial.',
+      value: 30.00
     },
-
-]
+    {
+        id: 5,
+        image: 'https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Bolo De Chocolate',
+        description: 'Bolo de chocolate com cobertura de brigadeiro e frutas e recheio de chocolate.',
+        value: 15.00
+      },
+      {
+        id: 6,
+        image: 'https://guiadacozinha.com.br/wp-content/uploads/2019/10/p%C3%A3o-queijo-recheado-catupiry.jpg',
+        title: 'Pão de Queijo',
+        description: 'Pão de queijo recheado com requeijão cremoso.',
+        value: 10.00
+      },
+  ]
